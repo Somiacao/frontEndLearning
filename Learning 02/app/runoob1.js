3fd7621ecc7eb54539c18043b005484e21877ebd
Somiacao <18210240048@fudan.edu.cn>
2019-08-04 21:38:04 +0800

    day2

---
+++ Learning 02/app/runoob1.js
@@ -0,0 +1,4 @@
+// document.write("It works.");
+// require("!style-loader!css-loader!./style.css");
+require("./style.css");
+document.write(require("./runoob2.js"));
---
+++ Learning 02/app/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: "./runoob1.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
+    }
+};
